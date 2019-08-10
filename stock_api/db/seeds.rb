d7d00b19fb65c6d87554cfa090b8c846559eba2c
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-08-10 12:52:02 -0400

    seeded db with companies

--- stock_api/db/seeds.rb
+++ stock_api/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Company.create([
+#   {name:"Coty Inc", symbol:'COTY'},
+#   {name:"Xeros Corporation", symbol: "XRX"},
+#   {name:"Chipotle Mexican Grill", symbol: "CMG"},
+#   {name:"Cadence Design Systems", symbol:"CDNS"},
+#   {name: "Anadarko Petroleum Corporation", symbol:"APC"},
+#   {name:"Total System Services", symbol: "TSS"},
+#   {name:"Dentsply Sirona Inc", symbol:"XRAY"},
+#   {name:"Advanced Micro Devices", symbol:"AMD"},
+#   {name:"MSCI", symbol:"MSCI"},
+#   {name:"DISH Network Corporation", symbol:"DISH"}
+#   ])
